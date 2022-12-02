91d3adc94ee0c23adaf836aabfa9e9a1d66b09fc
Michał Szczepański <siematumichael@protonmail.com>
2022-12-02 19:11:58 +0100

    Consolidate TUI additional options configuration part

--- additional_configs.py
+++ additional_configs.py
@@ -1,7 +1,7 @@
 import json as j
 import os
 
-INSTANCE_CONFIG_PATH: str = os.path.join("config", "ad_config.json")
+INSTANCE_CONFIG_PATH: str = os.path.join(os.getcwd(), "config", "ad_config.json")
 ALLOWED_ANSWERS: list[str] = ["bg", "ev", "lw"]
 
 # Determing How sentence words should looks
@@ -13,10 +13,16 @@ class WordsCaseMode():
 
     # Put to stdout all question required for this feature (defined in class) and obtain answers from stdin
     def _get_input_result() -> str:
-        return input("Determine how answers words should looks\n(bg - Begin sentence with upper case letter/ev - Each sentence word begin with uppercase/lw (default) - every sentence word begin with lower case letter):")
+        return input("Determine how answers words should looks\n(bg - Begin sentence with upper case letter/ev - Each sentence word begin with uppercase/lw (default) - every sentence word begin with lower case letter): ")
 
     def _check_conf_file_schema():
         pass
+
+    # Return parsed from json to dict configuration file content
+    def get_conf_file_parsed() -> dict:
+        file = open(INSTANCE_CONFIG_PATH, "r").read()
+        file = j.loads(file)
+        return file
 
     # Change existsing config to new or re-save config with new configuration datas
     def _retain_or_save_config(answer_sen_case_type: str):
@@ -31,11 +37,15 @@ class WordsCaseMode():
             #! Correct structure of config file must be maintained to swear working 
             # Setup word case type for answer sentences
             if len(answer_sen_case_type) > 0:
+                # Configure letter case type for each/begining word from answered sentence
                 conf_f_c["words_case_type"] = answer_sen_case_type
                 
                 # Save changed config datas to file
                 conf_f_c = j.dumps(conf_f_c, sort_keys=True, indent=4)
                 open(INSTANCE_CONFIG_PATH, "w").write(conf_f_c)
+
+                # Save configuration information for user
+                print("Configuration has been successfull saved!")
             else:
                 # send incorrect message to stdout
                 inc_stdout()
@@ -47,7 +57,7 @@ class WordsCaseMode():
     # Conjusction in order to get user answers for questions (using: _get_input_result()) and save it or update it in file (using: _retain_or_save_config(_))
     def process_input_result():
         # To Save/Re-save settings all setting file must not exists
-        if not os.path.exists(INSTANCE_CONFIG_PATH):
+        if os.path.exists(INSTANCE_CONFIG_PATH):
             default_answer = "lw"
             answer = WordsCaseMode._get_input_result()
             
@@ -61,6 +71,8 @@ class WordsCaseMode():
             else:
                 # Invalid answer: Try again to obtain duly answer
                 WordsCaseMode.process_input_result()
+        else:
+            print(f"Configuration file \"{INSTANCE_CONFIG_PATH}\" doesn't exists!. Create it with appropriate schema")
 
 # Run primitive test/tests
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import argparse
+import argparse, typing, re
 from get_set_login_data import get_login_data
 from instaling import start_instaling
-from additional_configs import ALLOWED_ANSWERS # Allowed answers for configuration arguments/commands
+from additional_configs import WordsCaseMode # site to configure additional options
 
 def add_arguments_and_commands(arg_parser_inst: argparse.ArgumentParser):
     # Run bot in different manner
     arg_parser_inst.add_argument("--run", help="Run bot. You must attach boolean \"true\" to run bot otherwise bot won't be launch (dichotomous scale = only two available options) (action is equal to don't any argument)", required=False, type=bool)
     
-    # Start bot configuration
-        # Run configuration
+    # Bot additional options configuration
+        # Start bot configuration
     config_com_params = {
         "help": "Accepted only boolean true to perform designed action. Configure options for bot such as words case type for answers sentences",
         "required": False,
@@ -16,13 +16,32 @@ def add_arguments_and_commands(arg_parser_inst: argparse.ArgumentParser):
     } 
     arg_parser_inst.add_argument("--config", **config_com_params)
     arg_parser_inst.add_argument("-c", **config_com_params)
+        # Display this configuration
+    display_addconfig_argsfor = {
+        "help": "Show actual additional options configuration. Required: true to perform action (accept only boolean values)",
+        "required": False,
+        "type": bool
+    }
+    arg_parser_inst.add_argument("--adconfshow", **display_addconfig_argsfor)
+    arg_parser_inst.add_argument("-ads", **display_addconfig_argsfor)
+
+
+# Check whether any argument has been passed durning launching the program
+# Return (boolean): "True" when argument has been passed or "False" in oposite site
+def check_some_argument_hb_passed(arguments: list[tuple[str, typing.Any]]) -> bool:
+    find: bool
+    for arg in arguments:
+        find = (arg[1] != None)
+        if find:
+            break
+    return find
 
 def main():
     # Configure argument parser
     parser_options = {
         "prog": "Awesome-Instaling the best instaling.pl bot",
         "description": "Bot which helps you in instaling.pl daily sessions",
-        "epilog": ":)"
+        "epilog": "Use command to configure/do specific action with bot or don't attach any argument to simply run bot :)"
     }
     arg_parser = argparse.ArgumentParser(**parser_options)
 
@@ -30,15 +49,33 @@ def main():
     add_arguments_and_commands(arg_parser_inst=arg_parser)
     
     # Parse args and process args depends on recived arg
-    parsed_args = arg_parser.parse_args() 
+    parsed_args = arg_parser.parse_args()
+    ind_any_arg_passed = check_some_argument_hb_passed(parsed_args._get_kwargs())
 
-    if len(parsed_args._get_args()) == 0 or ("--run" in parsed_args and parsed_args["--run"]):
+    if (not ind_any_arg_passed) or ("run" in parsed_args and parsed_args.run == True):
+        # Run bot when all allowed arguments haven't been passed or has been passed "--run" argument with assigned boolean "true" value
         login, password = get_login_data().values() # get login data and quick destructure them
         start_instaling(login, password) # start instaling
-        # print("The login is: " + login + "\n" + "The password is: " + password)
     else:
-        # TODO:
-        print(parsed_args)
+        # Handle other supported arguments/commands
+        if ("c" in parsed_args and parsed_args.c) or ("config" in parsed_args and parsed_args.config):
+            # Run configuration for additional bot options
+            WordsCaseMode.run()
+        elif parsed_args.ads or parsed_args.adconfshow:
+            # Display actuall additional options configuration
+                # Obtain options values
+            file_content: dict = WordsCaseMode.get_conf_file_parsed()
+            res_for_output = ""
+            for (key, value) in file_content.items():
+                single_val = f"{key} = {value}\n"
+                res_for_output += single_val
+            res_for_output = re.split("\n$", res_for_output)[0]
+                # Display that values
+            print(f"Configured additional bot options are:\n\n{res_for_output}")
+        else:
+            # Unsupported arguments are handled automaticaly by "argparse" module
+            pass 
+        
 
 if __name__ == "__main__":
     main()
